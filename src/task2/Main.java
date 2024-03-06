3935dfeebd189a24a2b7d5f9525f7d4edd77e49f
Ruslan <ruslan.tretyak.94@gmail.com>
2024-03-06 23:49:21 +0300

    done task2

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -1,4 +1,52 @@
 package task2;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        printDayOfWeek(parseDate());
+    }
+    private static LocalDate parseDate() {
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate localDate = null;
+        do {
+            System.out.println("Введите дату в формате dd/MM/yyyy");
+            try {
+                localDate = LocalDate.parse(scanner.next(), dateTimeFormatter);
+            } catch (DateTimeParseException e) {
+                System.out.println("некорректный ввод");
+            }
+        } while (localDate == null);
+        return localDate;
+    }
+    private static void printDayOfWeek(LocalDate localDate) {
+        switch (localDate.getDayOfWeek().getValue()) {
+            case (1):
+                System.out.println("Понедельник");
+                break;
+            case (2):
+                System.out.println("Вторник");
+                break;
+            case (3):
+                System.out.println("Среда");
+                break;
+            case (4):
+                System.out.println("Четверг");
+                break;
+            case (5):
+                System.out.println("Пятница");
+                break;
+            case (6):
+                System.out.println("Суббота");
+                break;
+            case (7):
+                System.out.println("Воскресенье");
+                break;
+        }
+    }
 }
